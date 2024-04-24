7ff37dd50d707332a754a3635bceae9213c5ecaa
Eren Yıldırım <88587113+erenyildirimmm@users.noreply.github.com>
2024-04-24 21:27:20 +0300

    Update book.js

--- controllers/book.js
+++ controllers/book.js
@@ -64,7 +64,6 @@ export const postBook = async (req, res, next) => {
 export const updatedBook = async (req, res, next) => {
   const { id } = req.params;
   const errors = validationResult(req);
-  console.log(id);
   try {
     if (!errors.isEmpty()) {
       const error = new Error("Validation failed, entered data is incorrect");
